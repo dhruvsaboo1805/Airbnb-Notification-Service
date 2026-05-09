6f799aa3b5a44c9766df613ebce981178683f101
dhruvsaboo1805 <saboodhruv789@gmail.com>
2026-05-09 16:15:12 +0530

    resolved .env part

--- src/main/java/com/example/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/example/notification_service/NotificationServiceApplication.java
@@ -1,5 +1,6 @@
 package com.example.notification_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        // Set system properties from .env
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
